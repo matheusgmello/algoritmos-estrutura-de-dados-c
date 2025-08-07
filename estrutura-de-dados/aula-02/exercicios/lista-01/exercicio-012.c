50dd85f5e31d93fdda08b8286ae261cac620c8a9
Matheus Mello <matheusgmello1@gmail.com>
2025-08-07 20:36:17 -0300

    feat: adicionado primeira e segunda lista de exercicios de ponteiros

---
+++ estrutura-de-dados/aula-02/exercicios/lista-01/exercicio-012.c
@@ -0,0 +1,10 @@
+/**
+ * Escrever um programa que contenha uma função que recebe o nome de uma 
+disciplina do curso de Sistemas para Internet e um caractere avulso. Usando 
+ponteiros,  a função deve informar o endereço da primeira ocorrência deste 
+caractere no nome da disciplina (usar %x no printf). Igualmente, deve ser dada 
+uma notificação caso o caractere não seja encontrado
+ * 
+ */
+
+ 
